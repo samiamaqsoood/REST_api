dd7a6d93998f742f5c9e1de8a51f1e34e65afbcf
Samia Maqsood <samiamaqsoood@gmail.com>
2025-07-14 18:06:25 +0500

    patch and delete method

--- index.js
+++ index.js
@@ -2,11 +2,14 @@ const express = require("express");
 const fs = require("fs");
 const app = express();
 const PORT = 8000;
-const users = require("./MOCK_DATA.json");
+// const users = require("./MOCK_DATA.json");
+const users = JSON.parse(fs.readFileSync("./MOCK_DATA.json", "utf-8"));
+
+const filePath = "./MOCK_DATA.json";
 
 // middleware-plugin to get data send by cient (in form format) in body
 app.use(express.urlencoded({extended: false}));
-app.use(express.json()); // ✅ Must-have for JSON bodies (POST, PATCH, DELETE)
+// app.use(express.json()); // ✅ Must-have for JSON bodies (POST, PATCH, DELETE)
 
 
 // render HTML on browser SSR
@@ -50,38 +53,84 @@ app.post("/api/users", (req, res) => {
 
 });
 
+// function getUsersFromFile() {
+//     const data = fs.readFileSync(filePath, "utf-8");
+//     return JSON.parse(data);
+// }
 app.patch("/api/users/:id", (req, res) => {
+    
     // edit data of an existing user
+    let usersToBEUpdates = users;
     const id = Number(req.params.id);
-    const userIndex = users.findIndex(user => user.id === id);
-
-    if (userIndex === -1) {
-        return res.status(404).json({ error: "User not found" });
-    }
+    const userIndex = usersToBEUpdates.find(user => user.id === id);
+    const userIndexN = usersToBEUpdates.findIndex(user => user.id === id);
 
     const body = req.body;
-    console.log("body", body);
-    const existingUser = users[userIndex];
+    usersToBEUpdates[userIndexN] = {}
+    usersToBEUpdates[userIndexN] = {...userIndex,...body}
 
-    users[userIndex] = {
-        ...existingUser, // Keep old fields
-        ...body          // Overwrite only what's sent
-    };
-
-    // users.push({id: user, ...body})
-    fs.writeFile("./MOCK_DATA.json",JSON.stringify(users, null, 2), (err,data) =>{
+     fs.writeFile("./MOCK_DATA.json" ,JSON.stringify(usersToBEUpdates), (err,data) =>{
         if (err) {
             console.error("File write error:", err);
             return res.status(500).json({ error: "Failed to update user" });
         }
         return res.json({status:"success", id:
             id});
-    })
+     })
+
+    // res.json({id: id, EXuserIndex: userIndex, UPDATEDuserInlist: users[userIndexN], body:body});
+
+    // const users = getUsersFromFile();
+    // const id = Number(req.params.id);
+    // const userIndex = users.findIndex(user => user.id === id);
+    // console.log(userIndex);
+
+    // if (userIndex === -1) {
+    //     return res.status(404).json({ error: "User not found" });
+    // }
+
+    // const body = req.body;
+    // console.log("body", body);
+    // const existingUser = users[userIndex];
+    // console.log(existingUser);
+
+    // users[userIndex] = {
+    //     ...existingUser, // Keep old fields
+    //     ...body          // Overwrite only what's sent
+    // };
+    // const newUsers = users;
+    // fs.writeFile(filePath ,JSON.stringify(newUsers), (err,data) =>{
+    //     if (err) {
+    //         console.error("File write error:", err);
+    //         return res.status(500).json({ error: "Failed to update user" });
+    //     }
+    //     return res.json({status:"success", id:
+    //         id});
+    // })
 });
 
 app.delete("/api/users/:id", (req, res) => {
     // delete data of an existing user
-    res.json({status:"pending"});
+
+    // res.json({users:users})
+    const id = Number(req.params.id);
+    let newUsers = users.filter(user => user.id !== id);
+
+    
+    if (newUsers.length === users.length) {
+        return res.status(404).json({ error: "User not found" });
+    }
+
+    fs.writeFile(filePath ,JSON.stringify(newUsers), (err,data) =>{
+        if (err) {
+            console.error("File write error:", err);
+            return res.status(500).json({ error: "Failed to update user" });
+        }
+        return res.json({status:"success", id:
+            id, newusers: newUsers});
+     })
+
+
 });
 
 // also for clear syntax we can define a route
